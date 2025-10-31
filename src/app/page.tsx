a7acab0a1e73e4dbe07483c74720d46bed24f5fd
palwan <palwansary1@gmail.com>
2025-10-31 16:50:08 +0500

    comit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,30 @@
+import AppAreaChart from "@/components/custom/charts/AppAreaChart";
+import AppBarChart from "@/components/custom/charts/AppBarChart";
+import AppPieChart from "@/components/custom/charts/AppPieChart";
 
-export default function Home() {
+const Homepage = () => {
    return (
-      <div className="font-sans grid ">
-         
+      <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-4 gap-4">
+         <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+            <AppBarChart />
+         </div>
+         <div className="bg-primary-foreground p-4 rounded-lg">
+            {/* <CardList title="Latest Transactions" /> */}
+         </div>
+         <div className="bg-primary-foreground p-4 rounded-lg">
+            <AppPieChart />
+         </div>
+         <div className="bg-primary-foreground p-4 rounded-lg">
+            {/* <TodoList /> */}
+         </div>
+         <div className="bg-primary-foreground p-4 rounded-lg lg:col-span-2 xl:col-span-1 2xl:col-span-2">
+            <AppAreaChart />
+         </div>
+         <div className="bg-primary-foreground p-4 rounded-lg">
+            {/* <CardList title="Popular Content" /> */}
+         </div>
       </div>
    );
-}
+};
+
+export default Homepage;
