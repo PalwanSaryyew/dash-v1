2498a61619f0ca05f6131a077465e07f9e2eac6d
Emeki <palwansary1@gmail.com>
2026-02-01 17:33:10 +0500

    middle

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,5 +3,6 @@
 export { default } from "next-auth/middleware";
 
 export const config = {
-   matcher: ["/((?!api/auth|_next/static|_next/image|favicon.ico).*)"],
+  // login sayfasını hariç tutarak diğer tüm sayfaları koru
+  matcher: ["/", "/topups", "/orders", "/users", "/products"],
 };
